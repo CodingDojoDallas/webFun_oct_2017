029f5f16de48a793b2cbcb3bad3853290645a9e4
Rodolfo Valdivieso <valdivieso_rodolfo@hotmail.com>
2017-10-23 13:09:01 -0500

    javascript assignment1

---
+++ Rodolfo_Valdivieso/JavaScript/assignment_js1.js
@@ -0,0 +1,32 @@
+
+var num1=10;
+var num2=25.32;
+var num3=2000000;
+var num4=-5;
+
+var str1='Hello';
+var str2='I';
+var str3='Am';
+var str4='A Ninja';
+
+var boll1=true;
+var boll2=false;
+
+var undef;
+
+console.log("The First Number:", num1);
+console.log("The Second Number:", num2);
+console.log("The Third Number:", num3);
+console.log("The Fourth Number:", num4);
+
+console.log("The First String:", str1);
+console.log("The Second String:", str2);
+console.log("The Third String:", str3);
+console.log("The Fourth String:", str4);
+
+console.log("The First Bollean:", boll1);
+console.log("The Second Bollean:", boll2);
+
+console.log("Undefined Var:", undef);
+
+
