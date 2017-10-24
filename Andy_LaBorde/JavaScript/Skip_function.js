0f5e090e72c48421923848a1f1347452f66cc266
Andy LaBorde <andikthegreat@gmail.com>
2017-10-23 21:41:29 -0500

    Skip function assignment

---
+++ Andy_LaBorde/JavaScript/Skip_function.js
@@ -0,0 +1,8 @@
+function skip(){
+	for(var i = 0; i <10 ; i+=2){
+		if(i < 10 && i > 0 ){
+			console.log(i);
+		}
+	}
+}
+skip();
--- Andy_LaBorde/JavaScript/pennies.js
+++ Andy_LaBorde/JavaScript/pennies.js
@@ -9,22 +9,22 @@ function endle$$(){
 	for(let i = 2; i <= 30; i++){
 		pennies *= 2;
 	}
-	console.log(`after 30 days the servant's reward will be $${pennies}`);
+	console.log(`After 30 days the servant's reward will be $${pennies}`);
 
 	for (let i = .01; i < 10000; i*=2){
 		day10k++;
 	}
-	console.log(`it took the servant ${day10k} to get $10,000`);
+	console.log(`It took the servant ${day10k} days to get $10,000.`);
 	
 	for(let i = .01; i <= 1000000000; i *= 2){
 		day1bil++;
 	}
-	console.log(`it took the servant ${day1bil} to get $1,000,000,000`);
+	console.log(`It took the servant ${day1bil} days to get $1,000,000,000.`);
 	
 	for(let i = 0.01; i < Infinity; i *= 2){
 		dayInfini++;
 	}
-	console.log(`It took the servant ${dayInfini} to get $$$Infinity$$$!!`);	
+	console.log(`It took the servant ${dayInfini} days to get $$$Infinity$$$!!`);	
 }
 endle$$();
 
