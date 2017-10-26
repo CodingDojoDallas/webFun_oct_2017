c2b2a28c5040451dfd8e3a0d32711dfe39baac81
Andy LaBorde <andikthegreat@gmail.com>
2017-10-25 20:31:33 -0500

    first_jQuery

---
+++ Andy_LaBorde/JavaScript/jQuery/optionalGroup/jscript.js
@@ -0,0 +1,34 @@
+$(document).ready(function(){
+	$('.box').hover(function(){
+		$(this).css('background-color', random());
+	}), $(this).css('background-color', random());
+	$('button.b1').click(function(){
+		$('.box').css('background-color', 'white');
+	}),
+	
+	 		$('button.b2').click(function(){
+	 		setInterval(timer, 100);
+		})
+		function timer(){
+			$('.box').each(function(){
+				$(this).css('background-color', random());
+		})
+
+	
+		
+
+}
+	function random(){
+	var arr = ['red','orange', 'yellow', 'green', 'blue', 'purple', 'pink']
+	// var arr = ['#ffffff','#345674', 'yellow', 'green', 'blue', 'purple', 'pink']
+	var color = arr[Math.floor(Math.random()*7)]
+	return color;
+	}
+
+
+});
+
+
+
+
+
---
+++ Andy_LaBorde/JavaScript/jQuery/optionalGroup/opt2.js
@@ -0,0 +1,26 @@
+// $ (document).ready(function(){
+
+// 	$('.box').hover(function(){
+	
+		
+// 		$(this).stop().animate( { color: 'hue' }, 500);
+// 	}, function() {
+// 		$(this).stop().animate( { color: 'hue'}, 500);
+// 	}
+// };
+
+	var arr=['red', 'orange', 'yellow', 'green', 'blue', 'purple', 'pink', 'teal'];
+		
+
+// 	}
+
+// }
+
+
+function getRandom(){
+		var hue = 'rgb(' + (Math.floor(Math.random() * 256)) + ',' + (Math.floor(Math.random() * 256)) + ',' + (Math.floor(Math.random() * 256)) + ')';
+		for(var i=0; i<arr.length; i++){
+}
+console.log(hue);
+}
+getRandom();
