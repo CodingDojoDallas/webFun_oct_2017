9fc597e626f1809d9ddb82314663b07ab81488ff
Brittney <b.nychole87@gmail.com>
2017-10-28 14:00:16 -0500

    contact card - incomplete

---
+++ brittney_Jackson/jQuery/Contact Card/script.js
@@ -0,0 +1,11 @@
+$(document).ready(function(){
+	
+
+	$(document).on("click", 'button', function(){
+		alert('this should work now');
+	});
+	// $('button').click(function(){
+	// 	$('form').submit(function{
+	// 		$(this).val()
+	// 	})
+});
